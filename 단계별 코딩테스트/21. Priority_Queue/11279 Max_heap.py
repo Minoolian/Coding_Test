45bc0c3886808502d869f6e4c9a995cf34460df8
이민우 <mw.lee@kakao.com>
2020-06-28 23:32:52 +0900

    단계별 코딩 [우선순위 큐]
    1. 최대 힙

---
+++ 단계별 코딩테스트/21. Priority_Queue/11279 Max_heap.py
@@ -0,0 +1,18 @@
+# 최대 힙
+
+# 배열에 자연수 x를 넣는다.
+# 배열에서 가장 큰 값을 출력하고, 그 값을 배열에서 제거한다.
+
+import heapq
+# 파이썬 모듈은 최소힙 heapq 를 지원한다. 최대 힙으로 변경해서 사용한다.
+import sys
+heap = []
+for _ in range(int(input())):
+    num = int(sys.stdin.readline())
+    if num != 0:
+        heapq.heappush(heap, (-num))
+    else:
+        if not heap:
+            print(0)
+        else:
+            print(-1 * heapq.heappop(heap))
