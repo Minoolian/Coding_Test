be70a7103986510da3e8bce41f75383f9d122479
이민우 <mw.lee@kakao.com>
2020-08-26 23:29:03 +0900

    단계별 코딩 [트리]
    1. 트리 순회: 딕셔너리를 이용하여 순회 표현.

---
+++ 단계별 코딩테스트/27. Tree/1991 Tree_traversal.py
@@ -0,0 +1,42 @@
+# 트리 순회
+
+# 전위 순회: (루트) (왼쪽 자식) (오른쪽 자식)
+# 중위 순회: (왼쪽 자식) (루트) (오른쪽 자식)
+# 후위 순회: (왼쪽 자식) (오른쪽 자식) (루트)
+
+import sys
+sys.setrecursionlimit(10**6)
+
+def pre(node): #전위 순회
+    if node=='.':
+        return
+    print(node, end="")
+    pre(tree[node][0])
+    pre(tree[node][1])
+
+def inorder(node): #중위 순회
+    if node=='.':
+        return
+    inorder(tree[node][0])
+    print(node, end="")
+    inorder(tree[node][1])
+
+def post(node): #후위 순회
+    if node==".":
+        return
+    post(tree[node][0])
+    post(tree[node][1])
+    print(node, end="")
+
+n=int(sys.stdin.readline())
+tree={} #딕셔너리를 이용하여 호출
+
+for _ in range(n):
+    root, left, right=sys.stdin.readline().split()
+    tree[root]=[left,right] #딕셔너리에 값을 저장
+
+pre('A')
+print()
+inorder('A')
+print()
+post('A')
