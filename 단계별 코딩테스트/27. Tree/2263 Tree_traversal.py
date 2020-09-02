3c4defe5f0fb391aa7d4073508daa4e1d5ac4902
이민우 <mw.lee@kakao.com>
2020-09-02 21:16:10 +0900

    단계별 코딩 [트리]
    1. 트리의 순회: 재귀를 이용한 풀이.

--- 단계별 코딩테스트/27. Tree/2263 Tree_traversal.py
+++ 단계별 코딩테스트/27. Tree/2263 Tree_traversal.py
@@ -4,5 +4,28 @@
 # 프리오더를 출력
 
 import sys
+sys.setrecursionlimit(10**6)
 
-if 
+def preorder(start_in, end_in, start_post, end_post):
+
+    if start_in>end_in or start_post>end_post:
+        return
+
+    root=postorder[end_post]
+    idx=find[root]
+
+    num=idx-start_in
+
+    print(root, end=" ")
+    preorder(start_in, idx-1, start_post, start_post+num-1)
+    preorder(idx+1, end_in, start_post+num, end_post-1)
+
+
+n=int(sys.stdin.readline())
+inorder=list(map(int,sys.stdin.readline().split()))
+postorder=list(map(int,sys.stdin.readline().split()))
+find=[0 for _ in range(n+1)] # 매번 .index() 로 검색하는 것은 비효율적.
+for i in range(n): # 전위 순회의 값을 검색하면 인덱스를 반환하는 리스트 작성.
+    find[inorder[i]]=i
+
+preorder(0,n-1,0,n-1)
