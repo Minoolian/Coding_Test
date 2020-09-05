f2adf66aabb374525b42cf3d12041bbb7bbb6ba8
이민우 <mw.lee@kakao.com>
2020-09-05 23:32:09 +0900

    단계별 코딩 [유니온 파인드]
    - 집합의 표현: Union Find 를 이용한 풀이 추가

--- 단계별 코딩테스트/28. Union_Find/1717 Set_representation.py
+++ 단계별 코딩테스트/28. Union_Find/1717 Set_representation.py
@@ -6,24 +6,64 @@
 #
 # 1로 시작하는 입력에 대해서 한 줄에 하나씩 YES/NO로 결과
 
+# Union Find를 이용한 풀이
 import sys
+input=sys.stdin.readline
 
-n,m=map(int,sys.stdin.readline().split())
+def find(x):
+    if parent[x]==x:
+        return x
+    parent[x]=find(parent[x])
+    return parent[x]
 
-a=[]
+def union(x,y):
+    x=find(x)
+    y=find(y)
+
+    if x!=y:
+        parent[y]=x
+
+def find_parent(x):
+    if parent[x]==x:
+        return x
+    return find_parent(parent[x])
+
+n,m=map(int,input().split())
+parent={}
+
 for i in range(n+1):
-    a.append(set([i]))
+    parent[i]=i
 
-lis=[list(map(int,sys.stdin.readline().split())) for _ in range(m)]
+for _ in range(m):
+    cmd,num_1,num_2=map(int,input().split())
 
-for i in lis:
-    if i[0]==0:
-        a[i[1]]=a[i[1]]|a[i[2]]
-        a[i[2]]=a[i[1]]|a[i[2]]
-    else:
-        if i[1]==i[2]:
-            print("YES")
+    if not cmd:
+        union(num_1,num_2)
+
+    if cmd:
+        if find_parent(num_1)==find_parent(num_2):
+            print('YES')
         else:
-            print("NO")
+            print('NO')
+# 메모리 초과
+# import sys
+#
+# n,m=map(int,sys.stdin.readline().split())
+#
+# a=[]
+# for i in range(n+1):
+#     a.append(set([i]))
+#
+# lis=[list(map(int,sys.stdin.readline().split())) for _ in range(m)]
+#
+# for i in lis:
+#     if i[0]==0:
+#         a[i[1]]=a[i[1]]|a[i[2]]
+#         a[i[2]]=a[i[1]]|a[i[2]]
+#     else:
+#         if i[1]==i[2]:
+#             print("YES")
+#         else:
+#             print("NO")
 
 
