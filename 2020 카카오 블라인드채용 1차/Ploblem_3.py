0cd5ad3fb880bc1dbe0b5d8d95d808677a336bf0
이민우 <mw.lee@kakao.com>
2020-09-10 20:29:41 +0900

    2020 카카오 블라인드 1차
    - 문제3: Rotate 함수, RLUD 함수 구현 (미완성)

--- 2020 카카오 블라인드채용 1차/Ploblem_3.py
+++ 2020 카카오 블라인드채용 1차/Ploblem_3.py
@@ -1,3 +1,19 @@
+
+def right():
+def left():
+def up():
+def down():
+
+def rotate(lis): # 90도 회전
+    n=len(lis[0])
+    new=[[None]*n for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            new[j][n-1-i]=lis[i][j]
+
+    return new
+
 def solution(key, lock):
     answer = True
     return answer
--- 단계별 코딩테스트/Test.py
+++ 단계별 코딩테스트/Test.py
@@ -1,14 +1,15 @@
 def rotate(lis):
     n=len(lis[0])
-    new=[[]*n for _ in range(n)]
+    new=[[None]*n for _ in range(n)]
 
     for i in range(n):
         for j in range(n):
-            new=lis[i][j]
+            new[j][n-1-i]=lis[i][j]
+
+    return new
 
 
 key=[[0, 0, 0], [1, 0, 0], [0, 1, 1]]
 lock=[[1, 1, 1], [1, 1, 0], [1, 0, 1]]
 
-key.
-print(key+lock)
+print(rotate(lock))
