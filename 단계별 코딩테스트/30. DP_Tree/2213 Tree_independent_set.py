be51064b9fef75536570bf2efd435602e240436f
이민우 <mw.lee@kakao.com>
2020-09-23 21:39:32 +0900

    단계별 코딩 [트리에서의 DP]
    - 트리의 독립집합: BFS와 DP를 이용한 풀이 (주석 추가)

--- 단계별 코딩테스트/30. DP_Tree/2213 Tree_independent_set.py
+++ 단계별 코딩테스트/30. DP_Tree/2213 Tree_independent_set.py
@@ -10,21 +10,27 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-w = [0] + list(map(int, input().split()))
-s = [[] for i in range(n + 1)]
+w = [0] + list(map(int, input().split())) # 정점 가중치 리스트
+s = [[] for i in range(n + 1)] # 간선 리스트
 dp = [[0] * 2 for i in range(n + 1)]
 visit = [False for i in range(n + 1)]
 num = [[[], []] for i in range(n + 1)]
+
+# dp[i][0]: 자신을 포함한, dp[i][1]: 자신을 포함하지 않은
 def dfs(start):
     visit[start] = True
+    # 초기 설정 및 반복 설정 (해당 정점에 대한 값 설정)
     dp[start][0] = w[start]
     num[start][0].append(start)
-    for i in s[start]:
-        if not visit[i]:
+    for i in s[start]: # 해당 정점과 연결된 정점에 대해서
+        if not visit[i]: # 방문하지 않았다면 (역행 방지)
             dfs(i)
-            dp[start][0] += dp[i][1]
-            for j in num[i][1]:
+            # 1. 해당 정점이 포함되면 다음 정점은 반드시 포함되지 않아야 한다.
+            dp[start][0] += dp[i][1] # 다음 정점이 포함되지 않은 최대값과의 해당 정점의 합
+            for j in num[i][1]: # 다음 정점이 포함되지 않은 집합
                 num[start][0].append(j)
+
+            # 2. 해당 정점이 포함되지 않으면 다음 정점의 두가지 경우 중 최대값을 반영한다.
             if max(dp[i][1], dp[i][0]) == dp[i][1]:
                 dp[start][1] += dp[i][1]
                 for k in num[i][1]:
@@ -33,11 +39,14 @@ def dfs(start):
                 dp[start][1] += dp[i][0]
                 for k in num[i][0]:
                     num[start][1].append(k)
-for i in range(n - 1):
+
+for i in range(n - 1): # 간선 리스트
     a, b = map(int, input().split())
-    s[a].append(b)
+    s[a].append(b) # 무방향
     s[b].append(a)
+
 dfs(1)
+
 if max(dp[1][0], dp[1][1]) == dp[1][0]:
     print(dp[1][0])
     a = num[1][0]
