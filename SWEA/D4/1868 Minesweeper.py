bd71398d0cb34bd51e988a13ff269c20f110a45c
이민우 <mw.lee@kakao.com>
2020-11-02 22:09:18 +0900

    SWEA [D4]
    - 파핑파핑 지뢰찾기: BFS를 이용한 풀이(미완)

--- SWEA/D4/1868 Minesweeper.py
+++ SWEA/D4/1868 Minesweeper.py
@@ -5,37 +5,71 @@
 
 from collections import deque
 
-n=int(input())
-lis=[list(map(str,input())) for _ in range(n)]
-mine=[[None]*n for _ in range(n)]
-visit=[[True]*n for _ in range(n)]
-
 def bfs():
-
     q=deque()
+    result=0
 
-    q.append([0,0])
+    for i in range(n):
+        for j in range(n):
+            
+            if visit[i][j] and conv[i][j]!=-1:
+                q.append([i,j])
+                while q:
+                    x,y=q.popleft()
 
-    while q:
-        x,y=q.popleft()
+                    if conv[x][y]!=-1 and visit[x][y]:
+                        visit[x][y]=False
 
-        m=0
-        for i in range(8):
-            nx=x+idx_x[i]
-            ny=y+idx_y[i]
+                        if conv[x][y]==0:
+                            for k in range(8):
+                                nx=x+idx_x[k]
+                                ny=y+idx_y[k]
+                                if nx>=0 and nx<n and ny>=0 and ny<n:
+                                    q.append([nx,ny])
+                        else:
+                            for k in range(8):
+                                nx=x+idx_x[k]
+                                ny=y+idx_y[k]
+                                if nx>=0 and nx<n and ny>=0 and ny<n and conv[nx][ny]==0 and visit[nx][ny]:
+                                    q.append([nx,ny])
 
-            if lis[nx][ny]=='*':
-                m+=1
-            if visit[nx][ny] and lis[nx][ny]!='*':
-                visit[nx][ny]=False
-                q.append([nx,ny])
+                result+=1
 
-        
+    return result
 
 
+if __name__=="__main__":
+    idx_x=[1,0,-1,0,1,-1,1,-1]
+    idx_y=[0,1,0,-1,1,-1,-1,1]
 
-idx_x=[1,0,-1,0,1,-1,1,-1]
-idx_y=[0,1,0,-1,1,-1,-1,1]
+    t=int(input())
+
+    for tc in range(1, t+1):
+        n=int(input())
+        lis=[list(map(str,input())) for _ in range(n)]
+        conv=[[None]*n for _ in range(n)]
+
+        mine=[[None]*n for _ in range(n)]
+        visit=[[True]*n for _ in range(n)]
+
+        for x in range(n):
+            for y in range(n):
+                m=0
+                if lis[x][y]=='*':
+                    conv[x][y]=-1
+                    continue
+
+                for i in range(8):
+                    nx=x+idx_x[i]
+                    ny=y+idx_y[i]
+
+                    if nx>=0 and nx<n and ny>=0 and ny<n:
+                        if lis[nx][ny]=='*':
+                            m+=1
+
+                conv[x][y]=m
+
+        print("#{} {}".format(tc,bfs()))
 
 # def dfs(x,y):
 #     m=0
