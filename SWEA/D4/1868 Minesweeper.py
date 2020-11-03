3a4305be911939e851d781f51f20ef2a01a535e8
이민우 <mw.lee@kakao.com>
2020-11-03 17:10:41 +0900

    SWEA [D4]
    - 파핑파핑 지뢰찾기: DFS를 이용한 풀이.

--- SWEA/D4/1868 Minesweeper.py
+++ SWEA/D4/1868 Minesweeper.py
@@ -1,41 +1,22 @@
 # 파핑파핑 지뢰찾기
 
-#
+# DFS를 이용한 풀이.
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LwsHaD1MDFAXc&categoryId=AV5LwsHaD1MDFAXc&categoryType=CODE
 
 from collections import deque
 
-def bfs():
-    q=deque()
-    result=0
+def dfs(x, y):
 
-    for i in range(n):
-        for j in range(n):
-            
-            if visit[i][j] and conv[i][j]!=-1:
-                q.append([i,j])
-                while q:
-                    x,y=q.popleft()
+    for i in range(8):
+        nx=x+idx_x[i]
+        ny=y+idx_y[i]
+        if nx>=0 and nx<n and ny>=0 and ny<n:
+            if visit[nx][ny] and conv[nx][ny]>0:
+                visit[nx][ny]=False
+            elif visit[nx][ny] and conv[nx][ny]==0:
+                visit[nx][ny]=False
+                dfs(nx, ny)
 
-                    if conv[x][y]!=-1 and visit[x][y]:
-                        visit[x][y]=False
-
-                        if conv[x][y]==0:
-                            for k in range(8):
-                                nx=x+idx_x[k]
-                                ny=y+idx_y[k]
-                                if nx>=0 and nx<n and ny>=0 and ny<n:
-                                    q.append([nx,ny])
-                        else:
-                            for k in range(8):
-                                nx=x+idx_x[k]
-                                ny=y+idx_y[k]
-                                if nx>=0 and nx<n and ny>=0 and ny<n and conv[nx][ny]==0 and visit[nx][ny]:
-                                    q.append([nx,ny])
-
-                result+=1
-
-    return result
 
 
 if __name__=="__main__":
@@ -48,15 +29,15 @@ if __name__=="__main__":
         n=int(input())
         lis=[list(map(str,input())) for _ in range(n)]
         conv=[[None]*n for _ in range(n)]
-
-        mine=[[None]*n for _ in range(n)]
         visit=[[True]*n for _ in range(n)]
+        q=deque()
 
         for x in range(n):
             for y in range(n):
                 m=0
                 if lis[x][y]=='*':
                     conv[x][y]=-1
+                    visit[x][y]=False
                     continue
 
                 for i in range(8):
@@ -69,25 +50,18 @@ if __name__=="__main__":
 
                 conv[x][y]=m
 
-        print("#{} {}".format(tc,bfs()))
+        fin=0
+        for x in range(n):
+            for y in range(n):
+                if conv[x][y]==0 and visit[x][y]:
+                    fin+=1
+                    visit[x][y]=False
+                    dfs(x,y)
 
-# def dfs(x,y):
-#     m=0
-#     for i in range(8):
-#         nx=x+idx_x[i]
-#         ny=y+idx_y[i]
-#         if lis[nx][ny]=='*':
-#             m+=1
-#     mine[x][y]=m
-#     visit[x][y]=False
-#
-#     if m==0:
-#         for i in range(4):
-#             nx=x+idx_x[i]
-#             ny=y+idx_y[i]
-#             if visit[nx][ny]:
-#                 dfs(nx,ny)
-#
-#     if not visit[x][y]:
-#
-#
+        for x in range(n):
+            for y in range(n):
+                if visit[x][y]:
+                    fin+=1
+
+        print("#{} {}".format(tc,fin))
+
