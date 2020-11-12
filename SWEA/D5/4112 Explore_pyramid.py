bdb6a224b5562b09819e5d566ff719cb5d1d6656
이민우 <mw.lee@kakao.com>
2020-11-12 16:51:23 +0900

    SWEA [D5]
    - 이상한 피라미드 탐험: BFS를 이용한 풀이.

--- SWEA/D5/4112 Explore_pyramid.py
+++ SWEA/D5/4112 Explore_pyramid.py
@@ -1,57 +1,52 @@
 # 이상한 피라미드 탐험
 
-#
+# BFS를 이용한 풀이
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWJHmLraeEwDFAUH&categoryId=AWJHmLraeEwDFAUH&categoryType=CODE
 
 
-import heapq as hq
+from collections import deque
 
-def dijkstra(start, flag):
-    dp[start]=0
-    hq.heappush(h,[0, start])
+def bfs(start):
+    q.append(start)
 
-    while h:
-        w, n=hq.heappop(h)
+    while q:
+        n=q.popleft()
 
         if n==t:
             return dp[t]
 
         for nn in pyramid[n]:
-            if nn>max(m,t):
+            if nn>t:
                 continue
-            if flag==1 and nn<n:
-                continue
-            if flag==0 and nn>n:
-                continue
-            nw=w+1
-            if nw<dp[nn]:
-                dp[nn]=nw
-                hq.heappush(h, [nw, nn])
+            if visit[nn] :
+                q.append(nn)
+                visit[nn]=False
+                dp[nn]=dp[n]+1
+
+
+pyramid=[None, [2,3]]
+num=2
+for i in range(2,150):
+    if num>10000:
+        break
+    for k in range(1, i+1):
+        if k==1:
+            pyramid.append([num-i+1, num+1, num+i, num+i+1])
+        elif k==i:
+            pyramid.append([num-i, num-1, num+i, num+i+1])
+        else:
+            pyramid.append([num-i, num-i+1, num-1, num+1, num+i, num+i+1])
+        num+=1
 
 t=int(input())
 for tc in range(1, t+1):
     m, t=map(int,input().split())
+    if m>t:
+        m, t=t, m
 
-    pyramid=[None, [2,3]]
-    dp=[100]*(max(m,t)+1)
-    h=[]
+    dp=[0]*(t+1)
+    visit=[True]*(t+1)
+    q=deque()
 
-    num=2
-    for i in range(2,450):
-        if num>max(m,t):
-            break
-        for k in range(1, i+1):
-            if k==1:
-                pyramid.append([num-i+1, num+1, num+i, num+i+1])
-            elif k==i:
-                pyramid.append([num-i, num-1, num+i, num+i+1])
-            else:
-                pyramid.append([num-i, num-i+1, num-1, num+1, num+i, num+i+1])
-            num+=1
-    if m>t: # UP
-        flag=0
-    else: # DOWN
-        flag=1
-
-    print("#{} {}".format(tc,dijkstra(m, flag)))
+    print("#{} {}".format(tc,bfs(m)))
 
