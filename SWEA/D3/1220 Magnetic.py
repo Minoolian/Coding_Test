0b48a04a1695a80ad5b5a9e6b9a8dc47fe4acb3a
Minoolian <mw.lee@kakao.com>
2020-11-20 19:57:47 +0900

    SWEA [D3]
    - Magnetic: 반복문을 이용한 풀이.

---
+++ SWEA/D3/1220 Magnetic.py
@@ -0,0 +1,21 @@
+# Magnetic
+
+#
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14hwZqABsCFAYD&categoryId=AV14hwZqABsCFAYD&categoryType=CODE
+
+for tc in range(1, 11):
+    int(input())
+    table=[list(map(int,input().split())) for _ in range(100)]
+
+    result=0
+    for i in range(100):
+        flag=0
+        for j in range(100):
+            if table[j][i]==1 and flag==0:
+                flag=1
+
+            if table[j][i]==2 and flag==1:
+                flag=0
+                result+=1
+
+    print("#{} {}".format(tc, result))
