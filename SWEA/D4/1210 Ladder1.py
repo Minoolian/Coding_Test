6777471da5e8f50ca49081c90ec05bfe33c64e83
Minoolian <mw.lee@kakao.com>
2020-12-12 17:18:38 +0900

    SWEA [D4]
    - Ladder1: DFS를 이용한 풀이

--- SWEA/D4/1210 Ladder1.py
+++ SWEA/D4/1210 Ladder1.py
@@ -4,16 +4,16 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14ABYKADACFAYh&categoryId=AV14ABYKADACFAYh&categoryType=CODE
 
 def dfs(x, y, dir):
-    if x==9:
+    if x==99:
         if maze[x][y]==2:
             return True
         else:
             return False
 
     if dir:
-        if dir==1 and y+1<10 and maze[x][y+1]:
+        if dir==1 and y+1<100 and maze[x][y+1]:
             y=y+1
-        elif dir==2 and x-1>0 and maze[x][y-1]:
+        elif dir==2 and y-1>=0 and maze[x][y-1]:
             y=y-1
         else:
             x=x+1
@@ -23,10 +23,10 @@ def dfs(x, y, dir):
         return result
 
     else:
-        if y+1<10 and maze[x][y+1]:
+        if y+1<100 and maze[x][y+1]:
             dir=1
             y=y+1
-        elif y-1>0 and maze[x][y-1]:
+        elif y-1>=0 and maze[x][y-1]:
             dir=2
             y=y-1
         else:
@@ -34,10 +34,12 @@ def dfs(x, y, dir):
         result=dfs(x,y,dir)
         return result
 
-maze=[list(map(int,input().split())) for _ in range(10)]
+for _ in range(10):
+    tc=int(input())
+    maze=[list(map(int,input().split())) for _ in range(100)]
 
-for i in range(10):
-    if maze[0][i]:
-        if dfs(0,i,0):
-            print(i)
-            break
+    for i in range(100):
+        if maze[0][i]:
+            if dfs(0,i,0):
+                print("#{} {}".format(tc,i))
+                break
