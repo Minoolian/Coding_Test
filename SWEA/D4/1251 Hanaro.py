12e9803d4e3b75a367262223f43e5b4533d0caea
Minoolian <mw.lee@kakao.com>
2021-01-04 17:29:40 +0900

    SWEA [D4]
    - 하나로: MST를 이용한 풀이 [kruskal / prims] (미완)

--- SWEA/D4/1251 Hanaro.py
+++ SWEA/D4/1251 Hanaro.py
@@ -1,5 +1,19 @@
 # 하나로
 
-# 다익스트라 알고리즘을 이용한 풀이.
+# MST 최소신장트리를 이용한 풀이.
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15StKqAQkCFAYD&categoryId=AV15StKqAQkCFAYD&categoryType=CODE
+from math import sqrt
 
+def policy(a,b,e):
+    L=sqrt(abs(x[a]-x[b])**2 + abs(y[a]-y[b])**2)
+    return e*(L**2)
+
+def prims(s):
+
+def kruskal():
+
+
+n=int(input())
+x=list(map(float,input().split()))
+y=list(map(float,input().split()))
+e=float(input())
