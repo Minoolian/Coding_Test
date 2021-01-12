b47a19990d720a45943a0d71b669e6063d88dd08
Minoolian <mw.lee@kakao.com>
2021-01-12 16:56:16 +0900

    SWEA [D4]
    - 지희의 고장난 계산기: DFS를 통한 풀이(완)

--- SWEA/D4/1808 Broken_calc.py
+++ SWEA/D4/1808 Broken_calc.py
@@ -1,20 +1,46 @@
 # 지희의 고장난 계산기
 
-#
+# DFS를 통해 약수를 구해나가는 풀이
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV4yC3pqCegDFAUx&categoryId=AV4yC3pqCegDFAUx&categoryType=CODE
 
-#
+def check(n):
+    for i in str(n):
+        if int(i) not in calc:
+            return False
+    return True
 
-def bfs():
+def solve(depth, cnt, q):
 
+    if q==1:
+        if cnt==0 and check(1):
+            return 1
+        return (depth-1)+cnt
 
-tmp=list(map(int,input().split()))
-calc=[]
-for i in range(len(tmp)):
-    if tmp[i]:
-        calc.append(i)
+    result=float('inf')
+    for d in range(2, int(q**0.5)+1):
+        if q%d==0 and check(d):
+            result=min(result,solve(depth+1, cnt+len(str(d)), q//d))
 
-fin=int(input())
+    if check(q):
+        result=min(result,len(str(q))+depth+cnt)
+
+    return result
+
+t=int(input())
+for tc in range(1, t+1):
+    tmp=list(map(int,input().split()))
+    calc=[]
+    for i in range(len(tmp)):
+        if tmp[i]:
+            calc.append(i)
+
+    fin=int(input())
+
+    re=solve(0,0, fin)+1
+    if re==float('inf'):
+        print(f"#{tc} {-1}")
+    else:
+        print(f"#{tc} {re}")
 
 # 잘못 접근한 방법 ( 수 X 수 로만 가는 방향)
 # import sys
