176311d5ac0e725557868f0fe9ba2e4d8adbd19e
Minoolian <mw.lee@kakao.com>
2021-01-17 23:00:59 +0900

    SWEA [D3]
    - 준환이의 운동관리: 조건문을 이용한 풀이

--- SWEA/D3/3431 Excercise.py
+++ SWEA/D3/3431 Excercise.py
@@ -1,6 +1,18 @@
 # 준환이의 운동관리
 
-#
+# 조건문을 이용한 풀이
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWE_ZXcqAAMDFAV2&categoryId=AWE_ZXcqAAMDFAV2&categoryType=CODE
 
-l,u,x=map(int,input().split())
+t=int(input())
+
+for tc in range(1, t+1):
+    l,u,x=map(int,input().split())
+
+    if l<=x<=u:
+        result=0
+    elif x<l:
+        result=l-x
+    else:
+        result=-1
+
+    print(f"#{tc} {result}")
