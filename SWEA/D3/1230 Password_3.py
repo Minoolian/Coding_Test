4e01d67584728261d6f8079d11495fb804060f73
Minoolian <mw.lee@kakao.com>
2021-01-28 15:26:22 +0900

    SWEA [D3]
    - 암호문3: 조건문을 이용한 풀이 (미완)

---
+++ SWEA/D3/1230 Password_3.py
@@ -0,0 +1,18 @@
+# 암호문3
+
+# 조건문을 이용한 풀이
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&problemLevel=4&problemLevel=5&contestProbId=AV14zIwqAHwCFAYD&categoryId=AV14zIwqAHwCFAYD&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=PYTHON&select-1=5&pageSize=30&pageIndex=2
+
+def calc(op):
+    if op=="I":
+
+
+
+n=int(input())
+pw=list[map(int,input().split())]
+c=int(input())
+
+for op in range(c):
+    if op=="I":
+    elif op=="D":
+    elif op=="A":
