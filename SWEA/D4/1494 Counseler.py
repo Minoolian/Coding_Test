4890420557a361b7c1a326389bc65d24574ec921
Minoolian <mw.lee@kakao.com>
2021-01-30 15:23:21 +0900

    SWEA [D4]
    - 사랑의 카운슬러: DFS를 이용한 풀이

--- SWEA/D4/1494 Counseler.py
+++ SWEA/D4/1494 Counseler.py
@@ -1,13 +1,41 @@
 # 사랑의 카운슬러
 
-#
+# DFS를 이용한 풀이(백트래킹)
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV2b_WPaAEIBBASw&categoryId=AV2b_WPaAEIBBASw&categoryType=CODE
-
-from itertools import permutations
 
 def v(a, b):
     return (a*a) + (b*b)
 
+def dfs(idx, cnt):
+    global result
+
+    if cnt==n//2:
+        x=0; y=0
+        for i in range(n):
+            if visit[i]:
+                x+=worm[i][0]
+                y+=worm[i][1]
+            else:
+                x-=worm[i][0]
+                y-=worm[i][1]
+
+        result=min(result, v(x,y))
+        return
+
+    for i in range(idx, n):
+        if visit[i]: continue
+
+        visit[i]=True
+        dfs(i+1, cnt+1)
+        visit[i]=False
+
 n=int(input())
 worm=[list(map(int,input().split())) for _ in range(n)]
+visit=[False]*n
+result=float('inf')
+
+dfs(0,0)
+print(result)
+
+
 
