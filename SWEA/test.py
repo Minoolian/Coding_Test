c6eb49c77d451450b80e61d59f5fdc11a8a8883b
Minoolian <mw.lee@kakao.com>
2021-05-08 17:52:57 +0900

    카카오 인턴십 코드 연습

--- SWEA/test.py
+++ SWEA/test.py
@@ -1,5 +1,25 @@
-from collections import Counter
+import re
 
+def toDeci(st):
+    value=str(st.group())
+    rep={'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    result=''
 
-a=Counter(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"])
-print(a["DIA"])
+    check=''
+    for s in value:
+        check+=s
+        if check not in rep.keys():
+            continue
+        else:
+            result+=rep[check]
+            check=''
+
+    return result
+
+def solution(s):
+
+    answer=re.sub('[a-z]+',toDeci,s)
+
+    return answer
+
+print(solution("one4seveneight"))
