cb728b03b992e93d8863cbc26610e8e607f9a335
Minoolian <mw.lee@kakao.com>
2021-05-26 19:11:32 +0900

    SWEA [D5]
    - 범준이의 제주도 여행 계획: DFS를 이용한 풀이 (시간초과)

--- SWEA/D5/1798 Travel_Schedule.py
+++ SWEA/D5/1798 Travel_Schedule.py
@@ -1,16 +1,52 @@
 # 범준이의 제주도 여행 계획
 
-#
+# dfs를 이용한 풀이
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&problemLevel=4&problemLevel=5&contestProbId=AV4x9oyaCR8DFAUx&categoryId=AV4x9oyaCR8DFAUx&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=PYTHON&select-1=5&pageSize=30&pageIndex=2
 
-n,m=map(int,input().split())
-r=[[None]*n for _ in range(n)]
-for i in range(n-1):
-    temp=list(map(int,input().split()))
-    for j in range(n-1,i,-1):
-        rr=temp.pop()
-        r[i][j]=rr
-        r[j][i]=rr
 
-info=[list(input().split()) for _ in range(n)]
+def dfs(cur,t,d,m,s,visit,result):
+    global fin
+
+    if d<m:
+        for i in range(1,n+1):
+            if info[i][0]=='A':
+                continue
+            if visit[i] and cur!=i:
+                if info[i][0]=='H' and t-r[cur][i]>=0:
+                    dfs(i, 540, d+1, m, s, visit, result+[i])
+                elif info[i][0]!='H' and t-r[cur][i]-int(info[i][1])>=10:
+                    visit[i]=False
+                    dfs(i, t-r[cur][i]-int(info[i][1]), d, m, s+int(info[i][2]), visit, result+[i])
+                    visit[i]=True
+    else:
+        for i in range(1,n+1):
+            if info[i][0]=='H':
+                continue
+            if visit[i] and cur!=i:
+                if info[i][0]=='A' and t-r[cur][i]>=0:
+                    if (fin[0]==s and len(fin[1])<len(result+[i])) or fin[0]<s:
+                        fin=[s,result+[i]]
+
+                elif info[i][0]!='A' and t-r[cur][i]-int(info[i][1])>=10:
+                    visit[i]=False
+                    dfs(i, t-r[cur][i]-int(info[i][1]), d, m, s+int(info[i][2]), visit, result+[i])
+                    visit[i]=True
+
+n=int(input())
+for tc in range(1,n+1):
+    n,m=map(int,input().split())
+    r=[[None]*(n+1) for _ in range(n+1)]
+    for i in range(n-1):
+        temp=list(map(int,input().split()))
+        for j in range(n-1,i,-1):
+            rr=temp.pop()
+            r[i+1][j+1]=rr
+            r[j+1][i+1]=rr
+
+    info=[None]+[list(input().split()) for _ in range(n)]
+    visit=[True]*(n+1)
+    fin=[0,[]]
+
+    dfs(1,540,1,m,0,visit,[])
+    print(fin)
 
--- SWEA/test.py
+++ SWEA/test.py
@@ -1,25 +1,48 @@
-import re
-
-def toDeci(st):
-    value=str(st.group())
-    rep={'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
-    result=''
-
-    check=''
-    for s in value:
-        check+=s
-        if check not in rep.keys():
+def dfs(rday, rmin, satis, last_visit):
+    found_points = False
+    for i, (m, s) in points.items():
+        if visited[i]:
             continue
+        if rmin - m - distance[last_visit][i] >= 10:
+            found_points = True
+            path.append(i)
+            visited[i] = 1
+            dfs(rday, rmin - m - distance[last_visit][i], satis + s, i)
+            path.pop()
+            visited[i] = 0
+    if not found_points:
+        if rday > 1:
+            for i in hotels:
+                if distance[last_visit][i] <= rmin:
+                    path.append(i)
+                    dfs(rday-1, 540, satis, i)
+                    path.pop()
         else:
-            result+=rep[check]
-            check=''
+            if rmin >= distance[last_visit][airport] and ans[0] < satis:
+                ans[0] = satis
+                ans[1] = path[1:] + [airport]
 
-    return result
 
-def solution(s):
-
-    answer=re.sub('[a-z]+',toDeci,s)
-
-    return answer
-
-print(solution("one4seveneight"))
+T = int(input())
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    distance = [[0]*(N+1) for _ in range(N+1)]
+    for i in range(1, N):
+        line = list(map(int, input().split()))
+        for j, d in enumerate(line, i+1):
+            distance[i][j] = distance[j][i] = d
+    hotels = []
+    points = {}
+    for i in range(1, N+1):
+        line = input()
+        if line[0] == 'P':
+            points[i] = list(map(int, line.split()[1:]))
+        elif line[0] == 'H':
+            hotels.append(i)
+        else:
+            airport = i
+    ans = [0, []]
+    visited = [0]*(N+1)
+    path = [airport]
+    dfs(M, 540, 0, airport)
+    print('#{} {} {}'.format(tc, ans[0], ' '.join(map(str, ans[1]))))
