bf4ec35b143f81a7ee09e2e75f186e744a35d761
Minoolian <mw.lee@kakao.com>
2021-11-01 18:13:19 +0900

    Baekjoon [골드1]
    - 괄호 추가하기3: DP를 이용한 풀이

--- Baekjoon/골드1/16639 Add_Braket_3.py
+++ Baekjoon/골드1/16639 Add_Braket_3.py
@@ -1,22 +1,27 @@
 # 괄호 추가하기 3
 
-#
+# DP를 이용한 풀이
 # https://www.acmicpc.net/problem/16639
 
 n=int(input())
 expression=input()
 
 l=n//2+1
-dp=[[[-float('inf'),float('inf')] if i!=j else [int(expression[i*2]),int(expression[i*2])] for i in range(l)] for j in range(l)]
+dp=[[[-2**31,2**31] if i!=j else [int(expression[i*2]),int(expression[i*2])] for i in range(l)] for j in range(l)]
 
 for j in range(1,l):
-    for i in range(l-i):
-        for k in range(i+1,)
-        temp=eval(''.join(expression[j:i+1]))
-        dp[i//2]=max(dp[i//2],eval(str(dp[(j-2)//2])+expression[j-1]+str(temp)))
+    for i in range(l-j):
+        for k in range(i,i+j):
+            for pre in dp[i][k]:
+                for post in dp[k+1][i+j]:
+                    temp=eval(str(pre)+expression[k*2+1]+str(post))
+                    dp[i][i+j][0]=max(dp[i][i+j][0],temp)
+                    dp[i][i+j][1]=min(dp[i][i+j][1],temp)
 
-print(dp[-1])
+            # temp=eval(''.join(expression[j:i+1]))
+            # dp[i//2]=max(dp[i//2],eval(str(dp[(j-2)//2])+expression[j-1]+str(temp)))
 
+print(dp[0][-1][0])
 # 실패 (테케는 모두 통과)
 # n=int(input())
 # expression=input()
