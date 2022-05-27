e0e5ad10053bd36dee9431c5285906ea6c8e14a1
minoolian <mw.lee@kakao.com>
2022-05-27 17:22:48 +0900

    Baekjoon [실버5, 브론즈2]
    * 재귀함수가 뭔가요?: 재귀를 이용한 풀이
    * 수열의 변화

---
+++ java_programming/src/baekjoon/chage_sequence/Main.java
@@ -0,0 +1,28 @@
+package baekjoon.chage_sequence;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), k = sc.nextInt();
+
+        String[] seqArr = sc.next().split(",");
+        int[] intArr = Arrays.stream(seqArr).mapToInt(Integer::parseInt).toArray();
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                intArr[j]=intArr[j+1]-intArr[j];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(intArr).limit(n-k).forEach(num -> sb.append(num + ","));
+
+        String result = sb.toString();
+        System.out.print(result.substring(0, result.length()-1));
+
+    }
+}
