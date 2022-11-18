9b024201b980958cf45e3bbbc775e9ba10eccabd
minoolian <mw.lee@kakao.com>
2022-11-18 14:42:34 +0900

    Baekjoon [골드4]
    * Longest Common Subsequence: DP를 이용한 풀이

---
+++ java_programming/src/baekjoon/LCS_5582/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.LCS_5582;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+        }
+
+
+        System.out.println(Arrays.stream(dp).mapToInt(ints -> Arrays.stream(ints).max().getAsInt()).max().getAsInt());
+    }
+}
